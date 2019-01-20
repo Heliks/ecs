72d4caf625093c7c9a50cf9dcf1a7bbe45d3dd54
felix <teh4.ews@gmail.com>
2019-01-20 18:59:00 +0100

    very important typo

--- src/types.ts
+++ src/types.ts
@@ -11,7 +11,7 @@ export interface EntityQuery {
 /**
  *
  * This is a workaround for bundling BitSet.js with webpack without getting
- * an import errors as the BitSet.d appears to not working correctly.
+ * an import errors as the BitSet.d appears to be not working correctly.
  * (webpack transpiles to ``new BitSet()`` to new ``bitset_1.BitSet()``
  * when ``bitset_1`` itself already contains the constructor).
  *
