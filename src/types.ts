ce6992038d37cc9215462542b0a9cfca20e095b9
Heliks <teh4.ews@gmail.com>
2019-06-20 22:39:37 +0200

    fix: class type constructor type hint

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
-export type ClassType<T = object> = new (...params: unknown[]) => T;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type ClassType<T = object> = new (...params: any[]) => T;
 export type ComponentType<T = object> = ClassType<T>;
 
 /** An entity is a unique symbol to which components can be attached to. */
