5b8a0e569dbba819b290f717f88c6f1a1662ce8e
felix <teh4.ews@gmail.com>
2023-09-15 15:10:39 +0200

    refactor(): use event-queue as peer dependency only

--- packages/ecs/src/index.ts
+++ packages/ecs/src/index.ts
@@ -3,6 +3,3 @@ export * from './entity';
 export * from './query';
 export * from './storage';
 export * from './world';
-
-// Export this for projects that don't implement this dependency themselves.
-export { EventQueue, Subscriber } from '@heliks/event-queue';
